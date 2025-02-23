ef59ee6359d369bd47a1f84ec787974ae0590d9e
Asadullah <asadullahabbasi225@gmail.com>
2025-02-23 12:38:16 +0500

    formatted

--- src/db/index.js
+++ src/db/index.js
@@ -1,21 +1,21 @@
-
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { DB_NAME } from '../constant.js';
-dotenv.config(); 
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+dotenv.config();
 
 const connectDb = async () => {
-    try {
-       const connection= await mongoose.connect(`${process.env.MONGODB_URI }/${DB_NAME}`);
-        console.log("Connected to MongoDB"+connection.connection.host);
-        console.log("running on port","http://localhost:"+process.env.PORT);
-        
-        return connection
-    } catch (error) {
-        console.log("Error at index.js line 8",error);
-        process.exit(1);    
-    }
-}
+  try {
+    const connection = await mongoose.connect(`${process.env.MONGODB_URI}`, {
+      dbName: DB_NAME, // Explicitly set the database name
+    });
+    console.log("Connected to MongoDB" + connection.connection.host);
+    console.log("running on port", "http://localhost:" + process.env.PORT);
 
-export default connectDb
+    return connection;
+  } catch (error) {
+    console.log("Error at index.js line 8", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
